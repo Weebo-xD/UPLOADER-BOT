f822a042969f85bd41e2a9a5f137cb0746080638
Ayush Deore <75108109+Weebo-xD@users.noreply.github.com>
2022-04-06 17:28:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1810130100:AAG9bhplU8-ubPzpg_mWCLZk9L4B_uaxvAU")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = int(os.environ.get("API_ID", 1995654))
+    API_HASH = os.environ.get("API_HASH", "91aa713a74d1bccb50cd1c03758316bf")
     # Get these values from my.telegram.org
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -24,8 +24,8 @@ class Config(object):
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "860540443"))
     SESSION_NAME = "UPLOADER-X-BOT"
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://userge:AlMs9AEDv5rQoehe@cluster1.1kyew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     MAX_RESULTS = "50"
